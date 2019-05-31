1346bc98aec5e1161665982ba6157464b9f42660
路明非 <wk1507341428@gmail.com>
2019-05-31 17:16:43 +0800

    环境变量配置完毕

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  mounted() {
+      console.log(process.env.NODE_ENV)
+      console.log(process.env.VUE_APP_T)
   }
-}
+};
 </script>
