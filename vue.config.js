db1c310742c989307fd0ab7b36a34c06bde23c2b
路明非 <wk1507341428@gmail.com>
2019-06-05 16:14:13 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -18,18 +18,22 @@ module.exports = {
             // 标题，可选项，一般情况不使用，通常是在路由切换时设置title
             // 需要注意的是使用title属性template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
             title: 'activity02 page',
+
+            chunks: ['chunk-vendors', 'chunk-common', 'activity02']
         },
         activity01: {
             entry: 'src/modules/activity01/main.js',
             template: 'public/activity01/index.html',
             filename: 'activity01/index.html',
-            title: 'activity01 page'          
+            title: 'activity01 page',
+            chunks: ['chunk-vendors', 'chunk-common', 'activity01']          
         },
         index: {
             entry: 'src/modules/index/main.js',
             template: 'public/index.html',
             filename: 'index.html',
-            title: '首页'          
+            title: '首页',
+            chunks: ['chunk-vendors', 'chunk-common', 'index']          
         },
     },
     productionSourceMap: false,
