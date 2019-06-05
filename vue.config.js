46ee6d3429d37d9967cb56ca8c7b950921bdb9f9
路明非 <wk1507341428@gmail.com>
2019-06-05 16:01:01 +0800

    up

--- vue.config.js
+++ vue.config.js
@@ -10,10 +10,10 @@ module.exports = {
             entry: 'src/modules/activity02/main.js',
 
             // 应用的模版，相当于单页面应用的public/index.html，可选项，省略时默认与模块名一致
-            template: 'public/activity02/activity02.html',
+            template: 'public/activity02/index.html',
 
             // 编译后在dist目录的输出文件名，可选项，省略时默认与模块名一致
-            filename: 'activity02.html',
+            filename: 'activity02/index.html',
 
             // 标题，可选项，一般情况不使用，通常是在路由切换时设置title
             // 需要注意的是使用title属性template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
@@ -21,11 +21,16 @@ module.exports = {
         },
         activity01: {
             entry: 'src/modules/activity01/main.js',
-            template: 'public/activity01/activity01.html',
-            filename: 'activity01.html',
+            template: 'public/activity01/index.html',
+            filename: 'activity01/index.html',
             title: 'activity01 page'          
         },
-        index: "src/modules/index/main.js"
+        index: {
+            entry: 'src/modules/index/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: '首页'          
+        },
     },
     productionSourceMap: false,
     css:{
